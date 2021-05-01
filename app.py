794a30a73bd647cd1ff42f473539fb313fcd8b41
Luiz Fernando Bueno Rosa <luizfernando5687@gmail.com>
2021-05-01 18:55:03 -0300

    Agora o script dirá quando um semestre ainda não tem cronograma

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import requests
 import json
 from helpers import readJson, getSoup
 
+def isDataOnline(URL):
+    answer = True
+    soup = getSoup(URL)
+    warning_sign = soup.find_all("div", class_="alert alert-warning")
+    if ( len(warning_sign) != 0 ):
+        answer = False
+    return answer
+
 def parseDisciplines(URL):
     soup = getSoup(URL)
     discipline_spans = soup.find_all("span", class_="label label-default")
@@ -15,9 +23,9 @@ def parseDisciplines(URL):
 def getDisciplinesPerInstitute(disciplines, institute_url):
     disciplines_payload = parseDisciplines(institute_url)
     for discipline in disciplines:
-        is_found = 'disponível neste semestre'
+        is_found = 'está disponível neste semestre'
         if discipline not in disciplines_payload:
-            is_found = 'não disponível neste semestre'
+            is_found = 'não está disponível neste semestre'
         print("Disciplina %s %s" % (discipline, is_found))
 
 def getInstitutesPerSemester(institutes, semester_url):
@@ -27,12 +35,16 @@ def getInstitutesPerSemester(institutes, semester_url):
         getDisciplinesPerInstitute(institutes[institute], institute_url)
         print()
 
+
 def getSemestersData(config):
     for semester in config.keys():
         year = semester[:4]
         semester_index = semester[5]
         url_semester = "https://www.dac.unicamp.br/portal/caderno-de-horarios/%s/%s/S/G/" % (year,semester_index)
         print("Pegando dados do semestre: %s" % (semester))
+        if isDataOnline(url_semester) == False:
+            print("Caderno de horário ainda não disponibilizado")
+            continue
         getInstitutesPerSemester(config[semester], url_semester)
 
 def main():
